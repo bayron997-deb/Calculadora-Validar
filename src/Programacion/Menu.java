cd903f7133db836739781683ad67867066e99e75
Bayron <bayron.m.1999@gmail.com>
2020-05-03 19:23:12 -0400

    Se agrego en la clase Menu con un objeto que llama a la clase operaciones. además se creo un metodo menu que hasta ahora contine el despliegue de la informacion de la calculadora

--- src/Programacion/Menu.java
+++ src/Programacion/Menu.java
@@ -1,4 +1,19 @@
 package Programacion;
 
 public class Menu {
+    //Crear un objeto de la clase operaciones para usar los métodos
+    Operaciones ope = new Operaciones();
+
+    //Metodo que muestra el menú
+    public void menu(){
+        //variable para la seleccion del menu
+        int seleccion;
+
+        //menú de selección
+        System.out.println("Bienvenido a la calculadora, tenemos 7 opciones a elegir\n" +
+                "1.-Suma\n2.-Resta\n3.-Multiplicación\n" +
+                "4.-División\n5.-Mayor de 2 números\n6.-Calcular potencia de un número\n" +
+                "7.-Salir de la calculadora");
+        seleccion = ope.validarInt();
+    }
 }
