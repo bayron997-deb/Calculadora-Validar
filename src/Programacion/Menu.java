e86d0d42aaa873f1bf03698d145d4c6cc4c19172
Bayron <bayron.m.1999@gmail.com>
2020-05-03 19:29:15 -0400

    se creo el switch con los 7 casos, en cada caso se agrego la confirmacion de la seleccion y el llamado del metodo ingrese2numeros de la clase operaciones. se agrego un metodo de decoracion con fines de hacer un salto de linea con "----". por ultimo se agrego el metodo salir que esta en el caso 7 de el switch

--- src/Programacion/Menu.java
+++ src/Programacion/Menu.java
@@ -15,5 +15,71 @@ public class Menu {
                 "4.-División\n5.-Mayor de 2 números\n6.-Calcular potencia de un número\n" +
                 "7.-Salir de la calculadora");
         seleccion = ope.validarInt();
+
+        //se crea un switch para la seleccion de menu
+        switch (seleccion){
+            case 1:
+                System.out.println("Usted ha seleccionado suma");
+                ope.ingrese2Numeros();
+                linea();
+                break;
+            case 2:
+                System.out.println("Usted ha seleccionado resta");
+                ope.ingrese2Numeros();
+                linea();
+                break;
+            case 3:
+                System.out.println("Usted ha seleccionado multiplicación");
+                ope.ingrese2Numeros();
+                linea();
+                break;
+            case 4:
+                System.out.println("Usted ha seleccionado división");
+                ope.ingrese2Numeros();
+                linea();
+                break;
+            case 5:
+                System.out.println("Usted ha seleccionado elegir el mayor de 2 numeros");
+                ope.ingrese2Numeros();
+                linea();
+                break;
+            case 6:
+                System.out.println("Usted ha seleccionado potencia de un numero");
+                ope.ingreseNumExp();
+                linea();
+                break;
+            case 7:
+                salir();
+                linea();
+                break;
+            default:
+                System.out.println("La opción ingresada no existe");
+                linea();
+        }
+    }
+    //metodo para salir
+    private void salir() {
+        boolean valido = false;
+        int eleccion;
+        do {
+            System.out.println("¿Desea salir de la calculadora? 1=Sí 0=No");
+            eleccion = ope.validarInt();
+            switch (eleccion) {
+                case 1:
+                    Calculadora.seguir = false;//agregar en clase calculadora
+                    valido = true;
+                    break;
+                case 0:
+                    valido = true;
+                    break;
+                default:
+                    System.out.println("La opción ingresada no existe");
+            }
+        } while(!valido);
+    }
+
+    //decoracion
+    private void linea() {
+        System.out.println("------------------------------------------------");
     }
 }
