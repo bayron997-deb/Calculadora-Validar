6617ea4fb5192641898b5b14351c80c1ce8223d1
Luis <l.burgos02@ufromail.cl>
2020-05-04 19:30:12 -0400

    agregé metodos a la clase operaciones de victor

--- src/Programacion/Operaciones.java
+++ src/Programacion/Operaciones.java
@@ -51,4 +51,43 @@ public class Operaciones {
         Calculadora.x=validarDouble(" ");
         Calculadora.y=validarDouble(" exponente del ");
     }
+    // Método para mostrar el resultado
+    public void resultado(double num){
+        System.out.println("El resultado es " + num);
+    }
+
+    //Metodos de operaciones básicas, utilizarán las variables globales, son del tipo double.
+    public void suma(double x, double y){
+        resultado(x+y);
+    }
+    public void resta(double x, double y){
+        resultado(x-y);
+    }
+    public void multiplicacion(double x, double y){
+        resultado(x*y);
+    }
+    public void division(double x, double y){
+        if(y==0){
+            System.out.println("Error: No se puede dividir por 0"); // Caso indeterminado
+        }
+        else{
+            resultado(x/y);
+        }
+    }
+    public void mayor(double x, double y) {
+        if (x >= y) {
+            System.out.println("El número mayor es " + x);
+        } else {
+            System.out.println("El número mayor es " + y);
+        }
+    }
+    public  void potencia(double x, double y){
+        if(x==0 && y==0){
+            System.out.println("Error: 0 elevado a 0 está indeterminado"); // Caso indeterminado
+        }
+        else {
+            resultado(Math.pow(x, y));
+        }
+    }
+
 }
