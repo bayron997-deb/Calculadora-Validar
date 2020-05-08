52bf53edbe40262753a354a548007149d9573562
Luis <l.burgos02@ufromail.cl>
2020-05-08 18:21:53 -0400

    se agregó comentarios a los metodos que operan como la suma entre otros

--- src/Programacion/Operaciones.java
+++ src/Programacion/Operaciones.java
@@ -51,42 +51,49 @@ public class Operaciones {
         Calculadora.x=validarDouble(" ");
         Calculadora.y=validarDouble(" exponente del ");
     }
+
     // Método para mostrar el resultado
     public void resultado(double num){
         System.out.println("El resultado es " + num);
     }
 
     //Metodos de operaciones básicas, utilizarán las variables globales, son del tipo double.
-    public void suma(double x, double y){
-        resultado(x+y);
+
+    public void suma(double x, double y){//metodo que suma
+        resultado(x+y);//se realiza la operación suma dentro del método resultado
     }
-    public void resta(double x, double y){
-        resultado(x-y);
+
+    public void resta(double x, double y){//metodo que resta
+        resultado(x-y);//se realiza la operación resta dentro del método resultado
     }
-    public void multiplicacion(double x, double y){
-        resultado(x*y);
+
+    public void multiplicacion(double x, double y){//metodo que multiplica
+        resultado(x*y);//se realiza la operación multiplicación dentro del método resultado
     }
-    public void division(double x, double y){
+
+    public void division(double x, double y){//metodo que divide
         if(y==0){
             System.out.println("Error: No se puede dividir por 0"); // Caso indeterminado
         }
         else{
-            resultado(x/y);
+            resultado(x/y);//se realiza la operación división dentro del método resultado
         }
     }
-    public void mayor(double x, double y) {
+
+    public void mayor(double x, double y) {//metodo que ve cual es es numero mayor
         if (x >= y) {
             System.out.println("El número mayor es " + x);
         } else {
             System.out.println("El número mayor es " + y);
-        }
+        }//se define con operadores lógicos cual es mayor
     }
-    public  void potencia(double x, double y){
+
+    public  void potencia(double x, double y){// metodo que eleva el primer número al segundo
         if(x==0 && y==0){
             System.out.println("Error: 0 elevado a 0 está indeterminado"); // Caso indeterminado
         }
         else {
-            resultado(Math.pow(x, y));
+            resultado(Math.pow(x, y));//se realiza la operación potencia dentro del método resultado con la funcion Math.pow de la clase Math.
         }
     }
 
